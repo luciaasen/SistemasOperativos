8e4abe9d9d77886314e94d98a0e79007d799d384
RodrigoDePool <rodrigodp05@gmail.com>
2017-02-10 17:05:36 +0100

    Cambios

--- APRENDIZAJE/rodrigo/ejercicio2.c
+++ APRENDIZAJE/rodrigo/ejercicio2.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+int main(){
+    pid_t val, p1, p2;
+    
+    val = fork();
+    if(val==0){
+        printf("Hola soy el proceso hijo\n");
+        exit(0);
+
+    }
+    waitpid(val,NULL,0);
+    printf("Hola soy el proceso padre\n");
+    return 2;
+}
---
+++ ENTREGABLE/practica1/ejercicio4SinHuerfanos.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define NUM_PROC 3
+
+int main(){
+    int pid, actual, padre;
+    int i;
+    
+    for(i=0; i<NUM_PROC;i++){
+        if((pid=fork())<0){
+            printf("Error al emplear fork \n");
+            exit(EXIT_FAILURE);
+        }else if(pid==0){
+            actual=getpid();
+            padre=getppid();
+            printf("Este proceso es el %d con padre %d\n",actual,padre);
+        }
+    }
+    /*El proceso con mas hijos tiene 3, con esto nos aseguramos que todos*/
+    /*los procesos finalizan antes de terminar la ejecucion de cualquier*/
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    exit(EXIT_FAILURE);
+}
