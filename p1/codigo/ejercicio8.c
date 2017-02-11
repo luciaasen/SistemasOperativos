a863f08e04026eb75e624d4167bfea688c3fb236
RodrigoDePool <rodrigodp05@gmail.com>
2017-02-11 19:41:58 +0100

    Hecho el ejercicio 8 y el makefile, falta probar mas profundamente el ejercicio8

--- p1/codigo/ejercicio8.c
+++ p1/codigo/ejercicio8.c
@@ -4,20 +4,69 @@
 #include <unistd.h>
 #include <string.h>
 
+void execute(char *func, char *flag);
 
-int main (void){
-    int pid;
-    char *cadena = (char *) malloc(sizeof(char)*TAM);
-
-    pid = fork();
-    if(pid==0){
-        printf("Introduzca un nombre: ");
-        scanf("%s",cadena);
-        exit(EXIT_SUCCESS);
+int main (int argc, char *argv[]){
+    int procs;
+    int i;
+    if(argc < 3){
+        perror("Argumentos insuficientes\n");
+        exit(EXIT_FAILURE);
     }
-    /*Esperamos en el proceso padre a que se introduzca el nombre*/
-    wait(NULL);
-    printf("Podemos imprimir el string? %s\n",cadena);
-    free(cadena);
+    procs=argc - 2;
+    /*Ejecutamos todos los programas en procesos hijos*/
+    for(i=0;i<procs;i++){
+       if(fork()==0){
+          execute(argv[i+1],argv[argc-1]);
+          perror("Error en la funcion execute");
+          exit(EXIT_FAILURE);
+       } 
+    }
+    /*Esperamos a que todos los procesos hijos cierren*/
+    for(i=0;i<procs;i++){
+        wait(NULL);
+    }
     exit(EXIT_SUCCESS);
+
+}
+
+/**
+* Esta funcion ejecuta el programa con direccion func 
+* con el comando especificado en flag.
+*
+*/
+void execute(char *func, char *flag){
+    int l;
+    char *prog[2];
+    prog[0]=func;
+    prog[1]=(char *)NULL;
+    l=strlen(flag);
+    if(l==2){
+        switch(flag[1]){
+            case 'l':
+                execl(func,func,(char *)NULL);
+                break;
+            case 'v':
+                execv(func,prog);
+                break;
+            default:
+                return;
+        
+        }
+    }else if(l==3){
+        switch(flag[1]){
+            case 'l':
+                execlp(func,func,(char *)NULL);
+                break;
+            case 'v':
+                execvp(func,prog);
+                break;
+            default:
+                return;
+        
+        }
+    }
+
+    return;
+
 }
