a24e2ed2f8994e84a5e17fe07adbf09964fc5ec3
luciaasen <luciaasen@gmail.com>
2017-02-24 17:48:41 +0100

    mi 9 def

--- APRENDIZAJE/lucia/ejercicio9.c
+++ APRENDIZAJE/lucia/ejercicio9.c
@@ -5,20 +5,26 @@
  #include <string.h>
  #include <sys/types.h>
 
+/**
+ *  Funcion main del ejercicio 9
+    Pide por pantalla dos valores double que seran almacenados por el proceso padre.
+    El proceso padre genera cuatro hijos, a los que envua los valores escaneados a traves de un pipe.
+    Cada hijo realiza una operacion y envia el resultado al padre a traves de otro pipe.
+    El padre imprime cada uno de los resultados extraidos del pipe.
 
+*/
  int main(){
     pid_t hijos[4];
     int i, nbytes, status;
     int fd1[4][2], fd2[4][2];
     double op1, op2, res;
-    char operandos[10], resultado[150];
+    char operandos[10], resultado[150], operacion[10];
 
     printf("Introduce tus dos operandos separados por una coma, por ejemplo '5,6'\n");
-    scanf("%lf,%lf", &op1, &op2);
+    scanf("%s", operandos);
     
     for(i = 0; i < 4; i ++){
-
-        if(pipe(fd1[i]) < 0 || pipe(fd2[i] < 0)){
+        if(pipe(fd1[i]) < 0 || pipe(fd2[i]) < 0){
             perror("Tuberia fallida");
             exit(EXIT_FAILURE);
         }
@@ -41,19 +47,23 @@
             switch(i){
                 case 0:
                     res = op1 + op2;
+                    strcpy(operacion , "Suma");
                     break;
                 case 1:
                     res = op1 - op2;
+                    strcpy(operacion , "Resta");
                     break;
                 case 2:
                     res = op1 * op2;
+                    strcpy(operacion , "Producto");
                     break;
                 default:
                     res =  op1 / op2;
+                    strcpy(operacion , "Division");
             }
-            sprintf(resultado, "Datos enviados a través de la tubería por el proces %d.\n Operando 1: %.3lf. Operando 2: %.3lf. Suma: %.3lf",  getpid(), op1, op2, res);
+            sprintf(resultado, "Datos enviados a través de la tubería por el proces %d.\n Operando 1: %.3lf. Operando 2: %.3lf. %s:%.3lf",  getpid(), op1, op2, operacion, res);
             nbytes = strlen(resultado) + 1;
-            if(write(fd1[i][1], resultado, nbytes) < nbytes){
+            if(write(fd2[i][1], resultado, nbytes) < nbytes){
                 perror("Escritura incorrecta del resultado");
                 exit(EXIT_FAILURE);
             }else{
@@ -66,8 +76,6 @@
             close(fd1[i][0]);
             close(fd2[i][1]);
 
-
-            sprintf(operandos, "%.3lf,%.3lf", op1, op2);
             nbytes = 1 + strlen(operandos);
             if(write(fd1[i][1], operandos, nbytes) < 0){
                 perror("No se han escrito los operandos en 1");
@@ -79,8 +87,10 @@
                 exit(EXIT_FAILURE);
             }
             printf("%s\n", resultado);
-            return 0;
+                       
         }
     }
+
+    return 0;
     
  }
